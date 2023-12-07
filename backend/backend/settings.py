623477e03ca4f6e8ff31d9b371dc8afb68282bcc
charlie b <charles.buckley@mobelux.com>
2023-12-07 09:31:03 -0800

    api call working

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -128,5 +128,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 CORS_ORIGIN_WHITELIST = [
-     'http://localhost:3000',
+    'http://localhost:3000',
+    "http://127.0.0.1:5173",
 ]
+
+
+# 'http://localhost:3000',
+# 'http://localhost:8000/',
+# 'http://127.0.0.1:5173/',
+# 'http://127.0.0.1:8000/api/beer/',
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,50 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 import axios, { AxiosResponse } from 'axios';
 
+interface Beer {
+  id: number;
+  title: string;
+}
 
-
+interface ApiResponse {
+  data: Beer[];
+}
 
 function App() {
-  const [data, setData] = useState([]);
+  const apiUrl = 'http://127.0.0.1:8000/api/beer/';
+
+  const [beers, setBeers] = useState<Beer[]>([]);
 
   useEffect(() => {
-    refreshList();
+    async function fetchBeers(): Promise<void> {
+      try {
+        const response: AxiosResponse<ApiResponse> = await axios.get(apiUrl);
 
-    console.log('data', data)
+        if (response.status === 200) {
+          setBeers(response.data);
+        } else {
+          console.error('Error:', response.status, response.statusText);
+        }
+      } catch (error) {
+        console.error('An error occurred:', error);
+      }
+    }
+
+    fetchBeers();
   }, []);
-
-  const refreshList = () => {
-    axios
-      .get("/api/beer/")
-      .then((res) => console.log(res.data))
-      .catch((err) => console.log(err));
-  };
-
 
   return (
     <>
       <h1 className="text-red-400">This is a test</h1>
       <h3>hello</h3>
+
+      <ul>
+        {beers.map((beer) => (
+          <li key={beer.id}>{beer.title}</li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
